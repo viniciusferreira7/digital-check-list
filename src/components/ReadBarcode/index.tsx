80262efb657423eb2d25bf198f3c38cef2612e22
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-09-11 22:56:53 -0300

    feat(readbarcode): close collapse when next slide

--- src/components/ReadBarcode/index.tsx
+++ src/components/ReadBarcode/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { CheckListContext } from '../../contexts/CheckListContext'
 import { useDisclosure, Collapse } from '@chakra-ui/react'
 
@@ -9,18 +9,28 @@ import {
 } from './styles'
 
 export function ReadBarcode() {
-  const { currentCheckList, handleFindCheckList } = useContext(CheckListContext)
+  const { handleFindCheckList } = useContext(CheckListContext)
 
   const { isOpen, onToggle } = useDisclosure()
+  const [closeCollapse, setCloseCollapse] = useState(isOpen)
+
+  useEffect(() => {
+    if (isOpen) setCloseCollapse(true)
+    if (!isOpen) setCloseCollapse(false)
+
+    return () => {
+      setCloseCollapse(false)
+    }
+  }, [closeCollapse, setCloseCollapse, isOpen])
 
   return (
     <ReadBarcodeContainer>
       <ButtonContainer onClick={onToggle}>SCAN</ButtonContainer>
-      <Collapse in={isOpen} animateOpacity>
+      <Collapse in={closeCollapse} animateOpacity>
         <CameraContainer></CameraContainer>
       </Collapse>
 
-      <Collapse in={isOpen} animateOpacity>
+      <Collapse in={closeCollapse} animateOpacity>
         <input type="text" onChange={handleFindCheckList} />
       </Collapse>
     </ReadBarcodeContainer>
